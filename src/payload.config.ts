80feb701dbf70f4fad1d06e745b278dd03ad9e10
clintjeez <clintjeez@gmail.com>
2025-07-18 07:01:06 +0100

    chore: update wrangler.toml for Cloudflare Pages deployment

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,7 @@
 import { vercelPostgresAdapter } from '@payloadcms/db-vercel-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -38,5 +39,19 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: true, // Apply storage to 'media' collection
+      },
+      bucket: process.env.S3_BUCKET || '',
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.S3_SECRET || '',
+        },
+        region: 'auto', // Cloudflare R2 uses 'auto' as the region
+        endpoint: process.env.S3_ENDPOINT || '',
+      },
+    }),
   ],
 })
